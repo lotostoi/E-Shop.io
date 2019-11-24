208bb9769d68497141f398930a33e8b39f15beac
Alexander <lotos_toi@mail.ru>
2019-11-25 09:19:29 +1000

    аа

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     let d = document
-    
+
 
     // *********************** Slider ************************************ //
     // *********************** Slider ************************************ //
@@ -95,19 +95,19 @@ window.onload = function () {
     // ************************* Drop menus from block search *************************//
     // ************************* Drop menus from block search *************************//
 
-    d.querySelector('.search').addEventListener('click', workClickk) 
+    d.querySelector('.search').addEventListener('click', workClickk)
 
     function workClickk(evt) {
         if (evt.target.parentNode.className === "search__typeSort") {
-            d.querySelector('.search__paramasSort').classList.toggle('search__paramasSort-active') 
+            d.querySelector('.search__paramasSort').classList.toggle('search__paramasSort-active')
         }
         if (evt.target.parentNode.className === "search__paramasSort search__paramasSort-active") {
             d.querySelector('.search__valueSort').innerHTML = evt.target.innerHTML
-            d.querySelector('.search__paramasSort').classList.toggle('search__paramasSort-active') 
+            d.querySelector('.search__paramasSort').classList.toggle('search__paramasSort-active')
         }
 
         if (evt.target.parentNode.className === "search__contShips") {
-            d.querySelector('.search__listAreasShips').classList.toggle('search__listAreasShips-active') 
+            d.querySelector('.search__listAreasShips').classList.toggle('search__listAreasShips-active')
         }
         if (evt.target.parentNode.className === "search__listAreasShips search__listAreasShips-active") {
             d.querySelector('.search__areaship').innerHTML = evt.target.innerHTML
@@ -115,4 +115,95 @@ window.onload = function () {
         }
     }
 
+    // ********************************* Create HTML *************************************** //
+    // ********************************* Create HTML *************************************** //
+    // ********************************* Create HTML *************************************** //
+
+    let name = [] //list names
+    let id = [] //list id
+    let brand = [] // brands list
+    let price = [] // prices list
+    let country = [] // countrys list
+    let ship = [] //ships list
+    let img = [] // img list
+
+
+    class product {
+        constructor(title, price, brand, country, ship, img) {
+            this.name = title;
+            this.price = price;
+            this.brand = brand;
+            this.country = country;
+            this.ship = ship;
+            this.img = img
+        }
+        render () {
+            return `<div class="contItem">
+                        <img src="${this.img}" width="198" height="180" alt="imgProduct" class="contItem__img">
+                        <span class="contItem__name">${this.name}</span>
+                        <span class="contItem__price">${this.price}</span>
+                        <button class="contItem__button">add to cart</button>
+                    </div>`
+        }
+    }
+
+    class contProduct {
+        constructor(className) {
+            this.className = className;
+            this.arrElems = [];
+        } 
+        fetchProduct () {
+            var xhr
+            if (window.XMLHttpRequest) {
+              xhr = new XMLHttpRequest()
+            } else if (window.ActiveXObject) {          
+              xhr = new ActiveXObject("Microsoft.XMLHTTP")
+            }
+            xhr.open('GET', 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json', true)
+            xhr.send();
+             xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                console.log(xhr.responseText)
+                }
+              } 
+
+
+
+
+
+
+            this.arrElems = [
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"},
+                {name: "sameName", price: '$10', brand: 'samsung', country: 'USA', ship: 'free' , img: "build/img/Photo1.png"}
+            ] 
+        }
+        render () {
+             let listProduct=''
+            for (let i=0; i < this.arrElems.length; i++) {
+                let Product = new product (this.arrElems[i].name+(i+1),
+                                           this.arrElems[i].price,
+                                           this.arrElems[i].brand,
+                                           this.arrElems[i].country,
+                                           this.arrElems[i].ship,
+                                           this.arrElems[i].img) 
+                listProduct +=  Product.render()
+            }
+           d.querySelector(this.className).innerHTML = listProduct 
+        }
+    }
+
+    let pageShop = new contProduct ('.productsPage')
+    pageShop.fetchProduct()
+    pageShop.render()
+    console.log(pageShop)
 }
